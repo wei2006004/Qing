10a2909eb7046722c6b13af4211f6ae15ad34a29
lizhiwei <wei2006004@foxmail.com>
2017-04-13 18:21:22 +0800

    * 兵落子验证逻辑

--- app/src/main/java/com/vinson/qing/verify/BingVerify.java
+++ app/src/main/java/com/vinson/qing/verify/BingVerify.java
@@ -1,5 +1,6 @@
 package com.vinson.qing.verify;
 
+import com.vinson.qing.bean.Chess;
 import com.vinson.qing.play.BoardStatus;
 
 /**
@@ -14,6 +15,25 @@ public class BingVerify extends ChessVerify {
 
     @Override
     public boolean playVerify(BoardStatus status, int fromx, int fromy, int tox, int toy) {
+        if (status.hasChess(tox, toy)) {
+            return false;
+        }
+        if (getType() == Chess.TYPE_GREEN) {
+            if (Math.abs(fromx + fromy - tox - toy) == 1 && fromy <= toy) {
+                if (fromy <= 4 && fromx != tox) {
+                    return false;
+                }
+                return true;
+            }
+        } else {
+            if (Math.abs(fromx + fromy - tox - toy) == 1 && fromy >= toy) {
+                if (fromy >= 5 && fromx != tox) {
+                    return false;
+                }
+                return true;
+            }
+        }
+
         return false;
     }
 }
